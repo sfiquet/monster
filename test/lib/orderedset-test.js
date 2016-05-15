fb5cdcf036ce5f3fc9ef764117cc0d0cba77961e
Sylvie Fiquet <sfiquetdev@gmail.com>
2016-05-15 09:27:57 +0100

    Add OrderedSet object to simplify implementation of feats and skills

---
+++ test/lib/orderedset-test.js
@@ -0,0 +1,67 @@
+/* jshint node: true */
+"use strict";
+
+var expect = require('chai').expect,
+	OrderedSet = require('../../lib/orderedset');
+
+describe('OrderedSet', function(){
+	describe('Constructor', function(){
+		it('creates an empty OrderedSet object and initialises it correctly', function(){
+			var set = new OrderedSet();
+			expect(set.order).to.deep.equal([]);
+			expect(set.set).to.deep.equal({});
+		});
+
+		it('creates an OrderedSet object and initialises it from the correct JSON format', function(){
+			var data = [
+				{'name': 'one', 'number': 4}, 
+				{'name': 'two', 'string': 'asd'}, 
+				{'name': 'three', 'object': {'name': 'four', 'blah': '123'}}
+			];
+			var set = new OrderedSet(data);
+			expect(set.order).to.deep.equal(['one', 'two', 'three']);
+			expect(set.set).to.deep.equal({
+				'one': {'name': 'one', 'number': 4},
+				'two': {'name': 'two', 'string': 'asd'},
+				'three': {'name': 'three', 'object': {'name': 'four', 'blah': '123'}}
+				});
+		});
+
+		it('creates an empty OrderedSet object when the data is in the wrong format', function(){
+			// not an array
+			var set = new OrderedSet({'name': 'one', 'number': 4});
+			expect(set.order).to.deep.equal([]);
+			expect(set.set).to.deep.equal({});
+
+			// array items are not all objects
+			set = new OrderedSet(['one', 'two', 4, {}]);
+			expect(set.order).to.deep.equal([]);
+			expect(set.set).to.deep.equal({});
+
+			// no name properties in objects
+			var data = [
+				{'number': 4}, 
+				{'string': 'asd'}, 
+				{'object': {'name': 'four', 'blah': '123'}}
+			];
+			set = new OrderedSet(data);
+			expect(set.order).to.deep.equal([]);
+			expect(set.set).to.deep.equal({});
+		});
+	});
+
+	describe('getKeys', function(){
+		it('returns a copy of the order array', function(){
+			var data = [
+				{'name': 'one', 'number': 4}, 
+				{'name': 'two', 'string': 'asd'}, 
+				{'name': 'three', 'object': {'name': 'four', 'blah': '123'}}
+			];
+			var set = new OrderedSet(data);
+			var keys = set.getKeys();
+			expect(keys).to.deep.equal(set.order);
+			expect(keys).to.not.equal(set.order);
+		});
+	});
+});
+
